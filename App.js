f688b447206861c59d92d0d7cd98fa7d6607c2b5
Swaraj Rath <gulal009@gmail.com>
2020-12-13 22:30:43 +0100

    Initial Setup of health app

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import {NavigationContainer, StackActions} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Image} from 'react-native';
+import Home from './Pages/Home';
+import Mission from './Pages/Mission';
 import { StyleSheet, Text, View } from 'react-native';
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} options={{
+          headerTransparent: true,
+          headerLeft: () => (
+            <Image source={require('./assets/drawericon.png')} style={{}} />
+          ),
+          title: '',
+          headerRight: () => (
+            <Image source={require('./assets/user.png')} style={{ marginHorizontal: 20, marginTop: 40 }} />
+          )
+        }} />
+        <Stack.Screen name="Mission" component={Mission} options={{
+          headerTransparent: true,
+          headerLeft: () => (
+            <Image source={require('./assets/blackmenu.png')} style={{ marginHorizontal: 20, marginTop: 40 }} />
+          ),
+          title: '',
+          headerRight: () => (
+            <Image source={require('./assets/user.png')} style={{ marginHorizontal: 35, marginTop: 45 }} />
+          )
+        }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
