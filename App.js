e41c2fd7de4b1d88521b0b9a57c3cb2ad62ed3fd
swarajrath <gulal009@gmail.com>
2020-12-22 23:59:45 +0100

    Login and Signup screen

--- App.js
+++ App.js
@@ -1,55 +1,8 @@
-import React, {useEffect} from 'react';
-import { Image } from 'react-native';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import React from 'react';
+import Providers from './navigation';
 
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import OnboardingScreen from './screens/OnboardingScreen';
-import LoginScreen from './screens/LoginScreen';
-import AsyncStorage from '@react-native-async-storage/async-storage'
-
-
-const AppStack = createStackNavigator()
-
-
-export default function App() {
-
-  const [isFirstLaunch, setisFirstLaunch] = React.useState(null)
-
-  useEffect(() => {
-    AsyncStorage.getItem('alreadyLaunched').then(value => {
-      if (value == null) {
-        AsyncStorage.setItem('alreadyLaunched', 'true')
-        setisFirstLaunch(true)
-      } else {
-        setisFirstLaunch(false)
-      }
-    })
-  }, [])
-
-  if (isFirstLaunch == null) {
-    return null
-  } else if (isFirstLaunch == true) {
-    
-    return (
-      <NavigationContainer>
-        <AppStack.Navigator headerMode="none">
-          <AppStack.Screen name="Onboarding" component={OnboardingScreen} />
-          {/* <AppStack.Screen name="Login" component={OnboardingScreen} /> */}
-        </AppStack.Navigator>
-      </NavigationContainer>
-      
-    );
-  } else {
-    <LoginScreen />
-  }
-
+const App = () => {
+  return <Providers />;
 }
 
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
-  },
-});
+export default App;
