d6afd21d12feec91f6baa159b1c7c9092a03cd20
swarajrath <gulal009@gmail.com>
2020-12-31 18:08:37 +0100

    Screens Barebone

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,14 +3,55 @@ import { AuthContext } from '../navigation/AuthProvider'
 import { View, Text, StyleSheet } from 'react-native';
 //import FormButton from '../src/components/FormButton';
 import { Container } from '../styles/HomeStyles';
+import Home from './Home'
 
-const HomeScreen = () => {
-    const { user, logout } = useContext(AuthContext)
-    return (
-        <Container>
-            <Text>Welcome</Text>
-        </Container>
-    )
+
+
+
+export default class HomeScreen extends Component {
+    render() {
+        return (
+            <NavigationContainer>
+                <StatusBar bg={'#313131'} />
+                <Sidebar menu={<Menu />}>
+                    <Content />
+                </Sidebar>
+
+                <Stack.Navigator>
+                    <Stack.Screen name="Home" component={Home} options={{
+                        headerTransparent: true,
+                        headerLeft: () => (
+                            <Image source={require('./assets/drawericon.png')} style={{}} />
+                        ),
+                        title: '',
+                        headerRight: () => (
+                            <Image source={require('./assets/user.png')} style={{ marginHorizontal: 20, marginTop: 40 }} />
+                        )
+                    }} />
+                    <Stack.Screen name="Mission" component={Mission} options={{
+                        headerTransparent: true,
+                        headerLeft: () => (
+                            <Image source={require('./assets/blackmenu.png')} style={{ marginHorizontal: 20, marginTop: 40 }} />
+                        ),
+                        title: '',
+                        headerRight: () => (
+                            <Image source={require('./assets/user.png')} style={{ marginHorizontal: 35, marginTop: 45 }} />
+                        )
+                    }} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        )
+    }
 }
 
-export default HomeScreen
+
+// const HomeScreen = () => {
+//     const { user, logout } = useContext(AuthContext)
+//     return (
+//         <Container>
+//             <Text>Welcome</Text>
+//         </Container>
+//     )
+// }
+
+// export default HomeScreen
